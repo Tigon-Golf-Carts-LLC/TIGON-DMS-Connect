0902340f481ebbf3ab7b5dd1bdba37f1cd356806
Tigon Golf Carts <tigongolfcarts@gmail.com>
2026-02-11 09:36:42 -0500

    Add files via upload

---
+++ chimera/src/admin/used/Cart.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tigon\Chimera\Admin\Used;
+
+use WP_Error;
+
+class Cart extends \Tigon\Chimera\Abstracts\Abstract_Cart
+{
+    /**
+     * Check if input cart is valid
+     *
+     * @return true|WP_Error
+     */
+    protected function verify_data()
+    {
+        $this->sku = $this->cart['vinNo'] ? $this->cart['vinNo'] : $this->cart['serialNo'];
+        if (!$this->sku && !$this->cart['pid'])
+            return new WP_Error($this->cart['_id'] . ' No serialNo or vinNo defined.');
+
+        return true;
+    }
+
+    protected function field_overrides()
+    {
+    }
+}
